6375080f98a7a4d6f832b86b11f2b60a22f0730c
[yejinj] <80540445+yejinj@users.noreply.github.com>
2024-02-02 14:19:19 +0900

    [Delete] CRA Template file delete

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Health from "./components/Health";
 
+//main page
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* component */}
         <Health />
       </header>
     </div>
