7013e0cd4420e46ae067789c9f962b98ab416762
[yejinj] <80540445+yejinj@users.noreply.github.com>
2024-03-03 17:57:37 +0900

    [Fix] about page 문구 수정

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -8,13 +8,7 @@ const MainPage = () => {
   return (
     <div>
       <div>
-        <b>오늘 한것</b>
-      </div>
-
-      <div>
-        <b>오늘 할것</b>
-        - figma 보고 problems page 만들기
-        - 비밀번호 유효성 체크
+        This is About Page.
       </div>
     </div>
   );
