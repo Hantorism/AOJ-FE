a2de317c115610ee1cd471b6396557a8b84e67c1
[yejinj] <80540445+yejinj@users.noreply.github.com>
2024-03-03 22:46:23 +0900

    [Feat] problem detail page 생성

---
+++ src/components/pages/ProblemDetailPage.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import LoadUserInfo from "../../utils/LoadUserInfo";
+import "./ProblemDetailPage.css";
+import { Nav, NavItem, NavLink } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
+
+const ProblemDetailPage = () => {
+  LoadUserInfo();
+    return (
+    <div>
+      asd
+    </div>
+    
+  );
+};
+
+export default ProblemDetailPage;
