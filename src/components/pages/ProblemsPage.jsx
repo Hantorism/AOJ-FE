65abca437ec4460b32ed41f7ff165d549620779a
[yejinj] <80540445+yejinj@users.noreply.github.com>
2024-03-03 17:56:18 +0900

    [Chore] problem page css 연결

--- src/components/pages/ProblemsPage.jsx
+++ src/components/pages/ProblemsPage.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { ReactElement, useEffect, useState, useContext } from "react";
 import LoadUserInfo from "../../utils/LoadUserInfo";
+import "./ProblemPage.css"
 
-const ProblemsPage = () => {
+const ProblemsPage = ({columns, data}) => {
   LoadUserInfo();
-
+  
     return (
-    <div class="m-3 bg-dark text-white">
-      <div class="problemTitle" style={{"vertical-align": "center"}}>
-        <h3> Title </h3>
-        <li>
-          <ol> 1. [기초] A, B 출력하기</ol>
-          <ol> 2. </ol>
-          <ol> 3. </ol>
-        </li>
+    <div class=" text-white" >
+      <div class="problemTitle">
+        <div class="title"> Title </div>
+        <div class="title_list">
+          <a href="/"> [기초] A, B 출력하기</a> <br/>
+          <a href="/"> [기초] A, B 출력하기</a>
+        </div>
       </div>
     </div>
   );
