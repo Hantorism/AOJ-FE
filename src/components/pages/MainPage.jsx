2c7a31a808314e609cb0f7d31bc695cc09d0b109
zzzang12 <wadevs12@gmail.com>
2024-05-17 05:44:49 +0900

    [Chore] health check 예시 추가

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -3,18 +3,28 @@ import Context from "../contexts/Context";
 import LoadUserInfo from "../../utils/LoadUserInfo";
 
 const MainPage = () => {
-  const [isHealthy, setIsHealthy] = useState('Backend not working...');
+  const [isLive, setIsLive] = useState('Backend is not live...');
+  const [isReady, setIsReady] = useState('Backend is not ready...');
+  const [isHealthy, setIsHealthy] = useState('Backend is not healthy...');
   LoadUserInfo();
 
-  useEffect( () => {
-    console.log(process.env.REACT_APP_API_URL)
-    fetch(process.env.REACT_APP_API_URL + 'health')
+  useEffect(() => {
+    // console.log(process.env.REACT_APP_API_URL)
+    fetch('http://localhost:8080/api' + '/livez')
+      .then((response) => response.text())
+      .then((data) => setIsLive(data));
+    fetch('http://localhost:8080/api' + '/readyz')
+      .then((response) => response.text())
+      .then((data) => setIsReady(data));
+    fetch('http://localhost:8080/api' + '/health')
       .then((response) => response.json())
-      .then((data) => setIsHealthy(data));
+      .then((data) => setIsHealthy(data.message));
   }, []);
 
   return (
     <div>
+      <p>{isLive}</p>
+      <p>{isReady}</p>
       <p>{isHealthy}</p>
     </div>
   );
